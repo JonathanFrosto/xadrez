cb718aca39dd6c3ac076a95d9f4ed38565357947
Jonathan Anthony <jonataanthony@gmail.com>
2020-11-28 21:47:23 -0300

    Castling done

--- src/com/jonathanfrosto/xadrez/chesss/pieces/King.java
+++ src/com/jonathanfrosto/xadrez/chesss/pieces/King.java
@@ -2,12 +2,16 @@ package com.jonathanfrosto.xadrez.chesss.pieces;
 
 import com.jonathanfrosto.xadrez.boardgame.Board;
 import com.jonathanfrosto.xadrez.boardgame.Position;
+import com.jonathanfrosto.xadrez.chesss.ChessMatch;
 import com.jonathanfrosto.xadrez.chesss.ChessPiece;
 import com.jonathanfrosto.xadrez.chesss.Color;
 
 public class King extends ChessPiece {
-    public King(Board board, Color color) {
+    private ChessMatch chessMatch;
+
+    public King(Board board, Color color, ChessMatch chessMatch) {
         super(board, color);
+        this.chessMatch = chessMatch;
     }
 
     @Override
@@ -25,6 +29,11 @@ public class King extends ChessPiece {
         if (getBoard().positionExists(auxPosition) && canMove(auxPosition)){
             matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
         }
+    }
+
+    private boolean testRookCastling(Position position){
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
     }
 
     @Override
@@ -48,6 +57,32 @@ public class King extends ChessPiece {
         testMove(mat,auxPosition,1,-1);
         //SE
         testMove(mat,auxPosition,1,1);
+
+        // Specialmove castling
+        if (getMoveCount() == 0 && !chessMatch.isCheck()){
+            // Kingside Rook
+            Position posRook1 = new Position(position.getRow(), position.getColumn() + 3);
+            if (testRookCastling(posRook1)){
+                Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+                Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+                if (getBoard().piece(p1) == null && getBoard().piece(p2) == null){
+                    mat[p2.getRow()][p2.getColumn()] = true;
+
+                }
+            }
+
+            //Queenside Rook
+            Position posRook2 = new Position(position.getRow(), position.getColumn() - 4);
+            if (testRookCastling(posRook2)){
+                Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+                Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+                Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+                if (getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null){
+                    mat[p2.getRow()][p2.getColumn()] = true;
+                }
+            }
+        }
+
 
         return mat;
     }
